d354030d3672cb80191e1ebb799483e482c08d50
Vitor Silva <vitor.procont@gmail.com>
2020-06-02 20:41:33 -0300

    feat(sqlite): add criacao tabelas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import routes from "./routes";
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-    response.json([
-        'Vitor',
-        'Diego',
-        'Reginaldo',
-        'Gustavo'
-    ])
-})
+app.use(routes)
 
 app.listen(3333);
