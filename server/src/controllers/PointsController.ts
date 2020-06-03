b6a59254bd1a34d58905e01481d20e23230b9c00
Vitor Silva <vitor.procont@gmail.com>
2020-06-02 22:29:41 -0300

    feat: listagem por tipo de coleta e local

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController{
+    async index(request: Request, response: Response){
+        const { city, uf, items } = request.query;
+
+        const parsedItems = String(items)
+            .split(',')
+        .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+        .select('points.*');
+
+        return response.json(points);
+    }
+
     async show(request: Request, response: Response){
         const { id } = request.params;
 
@@ -18,7 +36,6 @@ class PointsController{
         
         return response.json({point, items});
     }
-
 
     async create(request: Request, response: Response){
         const {
@@ -57,6 +74,7 @@ class PointsController{
         })
     
         await trx('point_items').insert(pointItems);
+        await trx.commit();
         return response.json({id: point_id, ...point});
     }
 }
